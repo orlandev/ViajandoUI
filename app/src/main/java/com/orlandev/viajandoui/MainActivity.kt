c95cf10162294c2579ce1afdda32ed2a8873606b
Orlando Novas Rodriguez <38017009+orlandev@users.noreply.github.com>
2022-06-29 11:48:19 -0400

    ⚡️ Add color theme

--- app/src/main/java/com/orlandev/viajandoui/MainActivity.kt
+++ app/src/main/java/com/orlandev/viajandoui/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.orlandev.viajandoui.ui.theme.ViajandoUITheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,6 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "dfghjk")
+                    }
 
                 }
             }
