c608c90454e19a0da80df30f5ddd160231f55aac
Orlando Novas Rodriguez <38017009+orlandev@users.noreply.github.com>
2022-07-01 11:15:46 -0400

    ⚡️ ADD CardEvents

--- app/src/main/java/com/orlandev/viajandoui/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/orlandev/viajandoui/ui/screens/HomeScreen.kt
@@ -1,11 +1,13 @@
 package com.orlandev.viajandoui.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Event
 import androidx.compose.material.icons.filled.Newspaper
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -13,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -109,13 +112,88 @@ fun NewsItem(news: ViajandoNewsType) {
 
         }
         is ViajandoNewsType.Event -> {
-            Text(text = news.title)
+            CardEvents(news)
         }
         is ViajandoNewsType.Offer -> {
             Text(text = news.title)
         }
         is ViajandoNewsType.Alert -> TODO()
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardEvents(news: ViajandoNewsType.Event, onClick: () -> Unit = {}) {
+
+    val loadingImage = remember {
+        mutableStateOf(false)
+    }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable {
+                onClick()
+            },
+    ) {
+
+        Row(modifier = Modifier) {
+            AsyncImage(
+                modifier = Modifier
+                    .size(130.dp)
+                    .placeholder(
+                        visible = loadingImage.value,
+                        color = MaterialTheme.colorScheme.background,
+                        shape = RoundedCornerShape(4.dp),
+                        highlight = PlaceholderHighlight.shimmer(
+                            highlightColor = MaterialTheme.colorScheme.onPrimaryContainer.copy(
+                                alpha = 0.6f
+                            ),
+                        ),
+                    ),
+
+                onLoading = {
+                    loadingImage.value = true
+                },
+                onSuccess = {
+                    loadingImage.value = false
+                },
+                contentScale = ContentScale.Crop,
+                model = news.imageUrl, contentDescription = null
+            )
+            Row(modifier = Modifier.background(Color.Blue).height(130.dp).fillMaxWidth().padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+                Column(modifier = Modifier) {
+                    if (loadingImage.value) {
+                        LinePlaceholderShimmer(80.dp)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        LinePlaceholderShimmer(130.dp)
+
+                    } else {
+                        Text(
+                            text = news.title, style = MaterialTheme.typography.titleMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = news.subTitle, style = MaterialTheme.typography.titleSmall,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight().background(Color.Red)
+                        .padding(horizontal = 8.dp), contentAlignment = Alignment.BottomEnd
+                ) {
+                    Icon(imageVector = Icons.Default.Event, contentDescription = null)
+                }
+            }
+        }
+    }
+
 }
 
 
