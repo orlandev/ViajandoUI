5c4599c44e6b36bde102849af5fdba15d7f0150f
Orlando N. Rodriguez <onovas05@gmail.com>
2022-07-01 00:07:13 -0400

    :bug:  Add new dependencies and some changes in AboutScreen.kt

--- app/src/main/java/com/orlandev/viajandoui/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/orlandev/viajandoui/ui/screens/AboutScreen.kt
@@ -1,19 +1,26 @@
 package com.orlandev.viajandoui.ui.screens
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ContactPhone
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.orlandev.viajandoui.BuildConfig
@@ -21,10 +28,15 @@ import com.orlandev.viajandoui.R
 import com.orlandev.viajandoui.ui.theme.ViajandoUITheme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutScreen() {
+    val scrollState= rememberScrollState()
 
-    Column(Modifier.fillMaxSize()) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)) {
 
         Row(
             modifier = Modifier
@@ -61,7 +73,7 @@ fun AboutScreen() {
         )
 
         Spacer(modifier = Modifier.height(20.dp))
-        Divider(modifier = Modifier.padding(start = 30.dp))
+        Divider(modifier = Modifier.padding(horizontal = 30.dp))
 
         Text(
             modifier = Modifier
@@ -69,6 +81,105 @@ fun AboutScreen() {
                 .padding(start = 30.dp, top = 10.dp),
             text = stringResource(id = R.string.contact_support),
             style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp),
+            text = stringResource(id = R.string.contact_schedule),
+            style = MaterialTheme.typography.titleSmall
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        ) {
+
+            ContactItem(phone = stringResource(id = R.string.contact_support_phone)) {
+                //TODO MAKE A CALL
+            }
+
+            ContactItem(phone = stringResource(id = R.string.contact_support_phone2)) {
+                //TODO MAKE A CALL
+            }
+
+            ContactItem(phone = stringResource(id = R.string.contact_support_phone3)) {
+                //TODO MAKE A CALL
+            }
+
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        ) {
+
+            ContactItem(
+                icon = Icons.Outlined.MailOutline,
+                phone = stringResource(id = R.string.contact_email)
+            ) {
+                //TODO MAKE A CALL
+            }
+        }
+
+        
+        
+        
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Column(modifier = Modifier) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = stringResource(id = R.string.copyright),
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center
+
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = stringResource(id = R.string.company_name),
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center
+
+            )
+        }
+
+    }
+}
+
+@Composable
+fun CardSocialMedias() {
+    
+}
+
+@Composable
+fun ContactItem(icon: ImageVector = Icons.Outlined.Phone, phone: String, onClick: () -> Unit) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .clickable {
+                onClick()
+            },
+    ) {
+        Icon(
+            icon,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(start = 10.dp),
+            text = phone,
+            style = MaterialTheme.typography.bodyMedium
         )
     }
 }
